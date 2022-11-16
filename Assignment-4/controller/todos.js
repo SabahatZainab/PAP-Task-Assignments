2a472dd9dc16e7501ba7ee76e9d70f6e9260741b
Smernz <sabahatzainab65@gmail.com>
2022-11-16 18:01:17 +0500

    Assignment4

--- Assignment-4/controller/todos.js
+++ Assignment-4/controller/todos.js
@@ -1,19 +1,15 @@
 const { db } = require('../config'); //configuration
-const { responseApiFalse  } = require('../util'); //it is better to restructure util
-const { responseApiTrue  } = require('../util');
-const { responsePost  } = require('../util');
-const { responsePut  } = require('../util');
-const { responsePatch  } = require('../util');
-const { responseDelete  } = require('../util');
+const { responseApi  } = require('../util'); //it is better to restructure util
+
 
 
 exports.get = (req, res) => {
     const body = req.body;
     const args = db.todos[body.id];
     if(args.delete){
-     responseApiFalse(res, 400, {success: false, message: 'Task not found'});
+     responseApi(res, 400, {success: false, message: 'Task not found'});
     }else{
-      responseApiTrue(res, 200, {success: true, message: 'Task found'}); 
+      responseApi(res, 200, {success: true, message: 'Task found', todos: [db.todos]}); 
     
   }
   }
@@ -22,7 +18,7 @@ exports.get = (req, res) => {
     const body = req.body;
     db.todos.push(body); //for array we use push method
 
-    responsePost(res, 200, {success: true, message: 'Task Added', todos: [db.todos] }); //sirf ek pahli data show hn rahi hai
+    responseApi(res, 200, {success: true, message: 'Task Added', todos: [db.todos] }); 
     
   }
 
@@ -30,7 +26,7 @@ exports.get = (req, res) => {
     const body = req.body;
     db.todos[0] = body;
     const arg0 = db.todos[0];
-    responsePut(res, 200, {success: true, message: 'Task Updated', todos: [arg0] }); //correct
+    responseApi(res, 200, {success: true, message: 'Task Updated', todos: [arg0] }); 
      
   }
 
@@ -38,15 +34,15 @@ exports.get = (req, res) => {
     const body = req.body;
     const args = db.todos[body.id];
     db.todos[body.id].name = body.name;
-    responsePut(res, 200, {success: true, message: 'Task Name Updated', todos: [args] }); 
+    responseApi(res, 200, {success: true, message: 'Task Name Updated', todos: [args] }); 
   }
 
   exports.delete = (req,res) => {
     const body = req.body;
     if(db.todos[body.id].id === body.id){
       db.todos[body.id].delete = 'true';
-      responseDelete(res, 200, {success: true, message: 'Task deleted'}); //correct
+      responseApi(res, 200, {success: true, message: 'Task deleted'});
   }else{
-      responseDelete(res, 400, {success: false, message: 'Task not deleted'}); //correct
+      responseApi(res, 400, {success: false, message: 'Task not deleted'}); 
   }
   }
